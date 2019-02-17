be6d69e12df27e093e6d92ab90ed29ffb7e09669
Boris Yumatov <bumatov@gmail.com>
2019-02-17 15:24:09 +0400

    first commit

---
+++ shmidtzkidz.py
@@ -0,0 +1,8 @@
+import telebot
+
+bot = telebot.TeleBot("668245937:AAENN0gBGBEDqByPsuuL-pC1irigyeROz0M")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
